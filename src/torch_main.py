53e84515ed5640fcfa7562c02e82171765f24490
gunesevitan <gunesevitan@gmail.com>
2022-07-12 19:37:10 +0300

    torch main

---
+++ src/torch_main.py
@@ -0,0 +1,34 @@
+import argparse
+import yaml
+import pandas as pd
+
+import settings
+from torch_trainers import SemanticSegmentationTrainer
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_path', type=str)
+    parser.add_argument('mode', type=str)
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config_path, 'r'), Loader=yaml.FullLoader)
+    df_train = pd.read_csv(settings.DATA / 'train_metadata.csv')
+    df_train = df_train.merge(pd.read_csv(settings.DATA / 'folds.csv'), on='id', how='left')
+
+    if config['task'] == 'semantic_segmentation':
+
+        trainer = SemanticSegmentationTrainer(
+            dataset_parameters=config['dataset_parameters'],
+            model_parameters=config['model_parameters'],
+            training_parameters=config['training_parameters'],
+            transform_parameters=config['transform_parameters'],
+            post_processing_parameters=config['post_processing_parameters'],
+            persistence_parameters=config['persistence_parameters']
+        )
+
+    if args.mode == 'train':
+        trainer.train_and_validate(df_train=df_train)
+    elif args.mode == 'inference':
+        trainer.inference(df_train=df_train)

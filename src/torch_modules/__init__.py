6bb9be9d7ae8bef8e3da83070e423c1105991dc6
gunesevitan <gunesevitan@gmail.com>
2022-07-18 13:18:50 +0300

    monai

--- src/torch_modules/__init__.py
+++ src/torch_modules/__init__.py
@@ -1,6 +1,5 @@
 from torch.nn import *
 from segmentation_models_pytorch.losses import *
 
-from .monai_segmentation_models import *
 from .semantic_segmentation_models import *
 from .loss_functions import *
--- src/torch_modules/semantic_segmentation_models.py
+++ src/torch_modules/semantic_segmentation_models.py
@@ -1,14 +1,20 @@
 import torch.nn as nn
 import segmentation_models_pytorch as smp
+import monai.networks.nets
 
 
 class SemanticSegmentationModel(nn.Module):
 
-    def __init__(self, model_class, model_args):
+    def __init__(self, model_module, model_class, model_args):
 
         super(SemanticSegmentationModel, self).__init__()
 
-        self.model = getattr(smp, model_class)(**model_args)
+        if model_module == 'smp':
+            self.model = getattr(smp, model_class)(**model_args)
+        elif model_module == 'monai':
+            self.model = getattr(monai.networks.nets, model_class)(**model_args)
+        else:
+            raise ValueError('Invalid model_module')
 
     def forward(self, x):
 

73d5685d904f57e760840388bbd110c0a5ae2b50
gunesevitan <gunesevitan@gmail.com>
2022-07-18 12:57:51 +0300

    monai

--- src/torch_modules/__init__.py
+++ src/torch_modules/__init__.py
@@ -1,6 +1,6 @@
 from torch.nn import *
 from segmentation_models_pytorch.losses import *
 
-from monai_segmentation_models import *
+from .monai_segmentation_models import *
 from .semantic_segmentation_models import *
 from .loss_functions import *

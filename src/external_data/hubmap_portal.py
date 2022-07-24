6d013db16bafa097f7b3d97552d7a5b13f6b485d
gunesevitan <gunesevitan@gmail.com>
2022-07-24 14:49:29 +0300

    hubmap portal

---
+++ src/external_data/hubmap_portal.py
@@ -0,0 +1,59 @@
+import logging
+import sys
+from glob import glob
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import cv2
+
+sys.path.append('..')
+import settings
+
+
+if __name__ == '__main__':
+
+    dataset_path = settings.DATA / 'external_data' / 'HuBMAP_Portal'
+    image_filenames = sorted(glob(str(dataset_path / 'images' / '*.png')))
+    metadata = []
+
+    for image_filename in tqdm(image_filenames):
+
+        image_id = image_filename.split("/")[-1].split(".")[0]
+
+        image = cv2.imread(image_filename)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Extract metadata from image
+        image_r_mean = np.mean(image[:, :, 0])
+        image_r_std = np.std(image[:, :, 0])
+        image_g_mean = np.mean(image[:, :, 1])
+        image_g_std = np.std(image[:, :, 1])
+        image_b_mean = np.mean(image[:, :, 2])
+        image_b_std = np.std(image[:, :, 2])
+
+        metadata.append({
+            'id': image_id,
+            'organ': 'kidney',
+            'data_source': 'HuBMAP_Portal',
+            'stain': 'PAS',
+            'image_height': image.shape[0],
+            'image_width': image.shape[1],
+            'pixel_size': np.nan,
+            'tissue_thickness': np.nan,
+            'rle': np.nan,
+            'age': np.nan,
+            'sex': np.nan,
+            'image_r_mean': image_r_mean,
+            'image_r_std': image_r_std,
+            'image_g_mean': image_g_mean,
+            'image_g_std': image_g_std,
+            'image_b_mean': image_b_mean,
+            'image_b_std': image_b_std,
+            'mask_area': np.nan,
+            'image_filename': image_filename,
+            'mask_filename': np.nan
+        })
+
+    df_metadata = pd.DataFrame(metadata)
+    df_metadata.to_csv(dataset_path / 'metadata.csv', index=False)
+    logging.info(f'Saved metadata.csv to {dataset_path}')
+    logging.info(f'Dataset Shape: {df_metadata.shape} - Memory Usage: {df_metadata.memory_usage().sum() / 1024 ** 2:.2f} MB')

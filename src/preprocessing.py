2374afbfbb4b435bf93c6b4df3867ecda5b55318
gunesevitan <gunesevitan@gmail.com>
2022-07-26 14:54:48 +0300

    resize aspect ratio

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,6 +3,28 @@ import numpy as np
 import cv2
 
 
+def resize_with_aspect_ratio(image, longest_edge):
+
+    """
+    Resize image while preserving aspect ratio
+
+    Parameters
+    ----------
+    image (numpy.ndarray of shape (height, width, 3)): Image array
+    longest_edge (int): Number of pixels on the longest edge
+
+    Returns
+    -------
+    image (numpy.ndarray of shape (resized_height, resized_width, 3)): Resized image array
+    """
+
+    height, width = image.shape[:2]
+    scale = longest_edge / max(height, width)
+    image = cv2.resize(image, dsize=(int(width * scale), int(height * scale)), interpolation=cv2.INTER_NEAREST)
+
+    return image
+
+
 def crop_image(image, mask=None, crop_black_border=True, crop_background=True, verbose=False):
 
     """

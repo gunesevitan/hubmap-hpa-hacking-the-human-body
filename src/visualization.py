d2e45a4f558b24366eeb769d63cd9d32642bce68
gunesevitan <gunesevitan@gmail.com>
2022-07-30 20:10:12 +0300

    viz

--- src/visualization.py
+++ src/visualization.py
@@ -3,12 +3,15 @@ import pathlib
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
+import cv2
 import tifffile
 import matplotlib.pyplot as plt
 import seaborn as sns
+import albumentations as A
 
 import settings
 import annotation_utils
+import preprocessing
 
 
 def visualize_categorical_feature_distribution(df, categorical_feature, path=None):
@@ -49,17 +52,19 @@ def visualize_categorical_feature_distribution(df, categorical_feature, path=Non
         plt.close(fig)
 
 
-def visualize_annotations(image, rle_mask, polygons, metadata, path=None):
+def visualize_annotations(image, rle_mask, polygons, metadata, crop_black_border=False, crop_background=False, path=None):
 
     """
     Visualize image along with its annotations
 
     Parameters
     ----------
-    image (path-like str or numpy.ndarray of shape (height, width)): Image path relative to root/data or image array
+    image (path-like str or numpy.ndarray of shape (height, width, 3)): Image path relative to root/data or image array
     rle_mask (str): Run-length encoded segmentation mask string
     polygons (list of shape (n_polygons, n_points, 2)): Polygons
     metadata (dict): Dictionary of metadata used in the visualization title
+    crop_black_border (bool): Whether to crop black border or not
+    crop_background (bool): Whether to crop background or not
     path (path-like str or None): Path of the output file or None (if path is None, plot is displayed with selected backend)
     """
 
@@ -75,13 +80,24 @@ def visualize_annotations(image, rle_mask, polygons, metadata, path=None):
         # Raise TypeError if image argument is not an array-like object or a path-like string
         raise TypeError('Image is not an array or path.')
 
+    if rle_mask is not None:
+        mask = annotation_utils.decode_rle_mask(rle_mask=rle_mask, shape=image.shape[:2])
+        if metadata['data_source'] == 'HPA' or metadata['data_source'] == 'Hubmap':
+            mask = mask.T
+
+    image, mask = preprocessing.crop_image(
+        image=image,
+        mask=mask,
+        crop_black_border=crop_black_border,
+        crop_background=crop_background
+    )
+
     fig, axes = plt.subplots(figsize=(48, 20), ncols=3)
 
     axes[0].imshow(image)
     axes[1].imshow(image)
 
     if rle_mask is not None:
-        mask = annotation_utils.decode_rle_mask(rle_mask=rle_mask, shape=image.shape[:2]).T
         axes[1].imshow(mask, alpha=0.5)
         axes[2].imshow(mask)
 
@@ -97,17 +113,17 @@ def visualize_annotations(image, rle_mask, polygons, metadata, path=None):
         axes[i].tick_params(axis='x', labelsize=15, pad=10)
         axes[i].tick_params(axis='y', labelsize=15, pad=10)
 
-    axes[0].set_title('Image', size=25, pad=15)
-    axes[1].set_title('Image + Mask and Polygons', size=25, pad=15)
-    axes[2].set_title('Mask and Polygons', size=25, pad=15)
+    axes[0].set_title('Image', size=20, pad=15)
+    axes[1].set_title('Image + Mask and Polygons', size=20, pad=15)
+    axes[2].set_title('Mask and Polygons', size=20, pad=15)
 
     fig.suptitle(
         f'''
-        Image ID {metadata["id"]} - {metadata["organ"]} - {metadata["data_source"]} - {int(metadata["age"])} - {metadata["sex"]}
-        Image Shape: {metadata["img_height"]}x{metadata["img_width"]} - Pixel Size: {metadata["pixel_size"]}µm - Tissue Thickness: {metadata["tissue_thickness"]}µm
-        {int(metadata["n_polygons"])} Annotations'
+        Image ID {metadata["id"]} - {metadata["organ"]} - {metadata["data_source"]} - {metadata["age"]} - {metadata["sex"]}
+        Image Shape: {metadata["image_height"]}x{metadata["image_width"]} - Pixel Size: {metadata["pixel_size"]}µm - Tissue Thickness: {metadata["tissue_thickness"]}µm
+        {metadata["n_polygons"]} Annotations'
         ''',
-        fontsize=30
+        fontsize=25
     )
 
     if path is None:
@@ -124,7 +140,7 @@ def visualize_predictions(image, ground_truth, predictions, metadata, evaluation
 
     Parameters
     ----------
-    image (numpy.ndarray of shape (height, width)): Image array
+    image (numpy.ndarray of shape (height, width, 3)): Image array
     ground_truth (numpy.ndarray of shape (height, width)): Ground-truth mask array
     predictions (numpy.ndarray of shape (height, width)): Predictions mask array
     metadata (dict): Dictionary of metadata used in the visualization title
@@ -166,11 +182,59 @@ def visualize_predictions(image, ground_truth, predictions, metadata, evaluation
     fig.suptitle(
         f'''
         Image ID {metadata["id"]} - {metadata["organ"]} - {metadata["data_source"]} - {int(metadata["age"])} - {metadata["sex"]}
-        Image Shape: {metadata["img_height"]}x{metadata["img_width"]} - Pixel Size: {metadata["pixel_size"]}µm - Tissue Thickness: {metadata["tissue_thickness"]}µm
+        Image Shape: {metadata["image_height"]}x{metadata["image_width"]} - Pixel Size: {metadata["pixel_size"]}µm - Tissue Thickness: {metadata["tissue_thickness"]}µm
         Dice Coefficient: {dice_coefficient} - Intersection over Union: {intersection_over_union}
         ''',
         fontsize=30
     )
+
+    if path is None:
+        plt.show()
+    else:
+        plt.savefig(path)
+        plt.close(fig)
+
+
+def visualize_transforms(image, mask, transforms, path=None):
+
+    """
+    Visualize image along with its annotations and predictions
+
+    Parameters
+    ----------
+    image (numpy.ndarray of shape (height, width, 3)): Image array
+    mask (numpy.ndarray of shape (height, width)): Mask array
+    transforms (albumentations.Compose): Transforms to apply on image and mask if it is given
+    path (path-like str or None): Path of the output file or None (if path is None, plot is displayed with selected backend)
+    """
+
+    fig, axes = plt.subplots(figsize=(32, 20), ncols=2)
+    axes[0].imshow(image)
+
+    if mask is not None:
+        rgb_mask = np.moveaxis(np.stack([mask] * 3), 0, -1) * 255
+        axes[0].imshow(rgb_mask, alpha=0.5)
+        # Apply transforms to image and mask
+        transformed = transforms(image=image, mask=mask)
+        transformed_image = transformed['image']
+        transformed_mask = transformed['mask']
+        transformed_rgb_mask = np.moveaxis(np.stack([transformed_mask] * 3), 0, -1) * 255
+        axes[1].imshow(transformed_image)
+        axes[1].imshow(transformed_rgb_mask, alpha=0.5)
+    else:
+        # Apply transforms to image
+        transformed = transforms(image=image)
+        transformed_image = transformed['image']
+        axes[1].imshow(transformed_image)
+
+    for i in range(2):
+        axes[i].set_xlabel('')
+        axes[i].set_ylabel('')
+        axes[i].tick_params(axis='x', labelsize=15, pad=10)
+        axes[i].tick_params(axis='y', labelsize=15, pad=10)
+
+    axes[0].set_title(f'{image.shape} - Mean: {image.mean():.2f} - Std: {image.std():.2f}\nMin: {image.min():.2f} - Max: {image.max():.2f}', size=20, pad=15)
+    axes[1].set_title(f'{transformed_image.shape} - Mean: {transformed_image.mean():.2f} - Std: {transformed_image.std():.2f}\nMin: {transformed_image.min():.2f} - Max: {transformed_image.max():.2f}', size=20, pad=15)
 
     if path is None:
         plt.show()
@@ -270,7 +334,8 @@ if __name__ == '__main__':
     df_train = pd.read_csv(settings.DATA / 'train_metadata.csv')
 
     VISUALIZE_IMAGES = False
-    VISUALIZE_CATEGORICAL_FEATURES = True
+    VISUALIZE_CATEGORICAL_FEATURES = False
+    VISUALIZE_TRANSFORMS = False
 
     if VISUALIZE_IMAGES:
 
@@ -289,8 +354,10 @@ if __name__ == '__main__':
             visualize_annotations(
                 image=image,
                 rle_mask=rle_mask,
-                polygons=polygons,
+                polygons=None,
                 metadata=row.to_dict(),
+                crop_black_border=True,
+                crop_background=True,
                 path=annotations_visualizations_directory / f'{row["id"]}_annotations.png'
             )
 
@@ -309,3 +376,68 @@ if __name__ == '__main__':
             )
 
         logging.info(f'Saved categorical feature distribution visualizations to {settings.EDA}')
+
+    if VISUALIZE_TRANSFORMS:
+
+        image_idx = 0
+        image = tifffile.imread(df_train.loc[image_idx, 'image_filename'])
+        mask = annotation_utils.decode_rle_mask(df_train.loc[image_idx, 'rle'], shape=image.shape[:2]).T
+
+        transforms = A.Compose([
+            A.RandomResizedCrop(
+                height=512,
+                width=512,
+                scale=(0.5, 0.5),
+                ratio=(0.5, 1.5),
+                interpolation=cv2.INTER_NEAREST,
+                always_apply=True
+            ),
+            A.HorizontalFlip(p=0.0),
+            A.VerticalFlip(p=0.0),
+            A.RandomRotate90(p=0.0),
+            A.ShiftScaleRotate(
+                shift_limit=0.1,
+                scale_limit=0.25,
+                rotate_limit=180,
+                interpolation=cv2.INTER_NEAREST,
+                border_mode=cv2.BORDER_CONSTANT,
+                value=(245, 245, 245),
+                mask_value=(0, 0, 0),
+                p=0.0
+            ),
+            A.RGBShift(
+                r_shift_limit=20,
+                g_shift_limit=20,
+                b_shift_limit=20,
+                p=0.0
+            )
+
+        ])
+        visualize_transforms(
+            image=image,
+            mask=mask,
+            transforms=transforms
+        )
+
+    df_hubmap_kidney_segmentation_metadata = pd.read_csv(settings.DATA / 'hubmap_kidney_segmentation_metadata.csv')
+    df_hubmap_kidney_segmentation_metadata = df_hubmap_kidney_segmentation_metadata.loc[df_hubmap_kidney_segmentation_metadata['mask_area'] > 5000].reset_index(drop=True)
+
+
+    print(df_hubmap_kidney_segmentation_metadata.shape, df_hubmap_kidney_segmentation_metadata['mask_area'].mean(), df_hubmap_kidney_segmentation_metadata['mask_area'].min(), df_hubmap_kidney_segmentation_metadata['mask_area'].max())
+    idx = np.argmin(df_hubmap_kidney_segmentation_metadata['mask_area'].values)
+    image = cv2.imread(df_hubmap_kidney_segmentation_metadata.loc[idx, 'image_filename'])
+    mask = cv2.imread(df_hubmap_kidney_segmentation_metadata.loc[idx, 'mask_filename'], -1)
+
+
+    plt.imshow(image)
+    plt.imshow(mask, alpha=0.5)
+    plt.show()
+
+    idx = np.argmax(df_hubmap_kidney_segmentation_metadata['mask_area'].values)
+    image = cv2.imread(df_hubmap_kidney_segmentation_metadata.loc[idx, 'image_filename'], -1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    mask = cv2.imread(df_hubmap_kidney_segmentation_metadata.loc[idx, 'mask_filename'], -1)
+
+    plt.imshow(image)
+    #plt.imshow(mask, alpha=0.5)
+    plt.show()

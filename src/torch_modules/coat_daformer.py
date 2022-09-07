4b4d6c461f5192e31de00749fd5a27124de56b93
gunesevitan <gunesevitan@gmail.com>
2022-09-07 13:28:46 +0300

    coat daformer wrapper

--- src/torch_modules/coat_daformer.py
+++ src/torch_modules/coat_daformer.py
@@ -26,28 +26,7 @@ class CoaTDAFormer(nn.Module):
     def forward(self, x):
 
         x = self.encoder(x)
-        for a in x:
-            print(a.shape)
-
         last, decoder = self.decoder(x)
         out = self.conv_head(last)
 
         return out
-
-
-if __name__ == '__main__':
-
-    m = CoaTDAFormer(
-        encoder_name='coat_lite_medium',
-        decoder_args={
-            'encoder_dim': (128, 256, 320, 512),
-            'decoder_dim': 256,
-            'dilation': (1, 6, 12, 18),
-            'use_bn_mlp': True,
-            'fuse': 'conv3x3'
-        },
-        encoder_weights='../../models/coat/coat_lite_medium.pth',
-
-    )
-
-    x = torch.rand(1, 3, 224, 224)
